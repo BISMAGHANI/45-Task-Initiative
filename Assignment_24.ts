2335328d28a379e0f4f7ab77b18835d7002969c0
BISMAGHANI <159704908+BISMAGHANI@users.noreply.github.com>
2024-04-28 22:06:28 +0500

    Add files via upload

---
+++ Assignment_24.ts
@@ -0,0 +1,43 @@
+// Equality and Unequalty 
+console.log("Equality Test with strings: ", "Apple" === "Apple");
+
+// Equality and Unequalty 
+console.log("Inequality Test with strings: ", ("Apple" as string) != "Orange");
+
+// • Tests using the lower case function
+console.log("Lower case function test: ", "HELLO".toLowerCase() === "hello")
+
+// • Numerical tests involving equality 
+console.log("Equality Test with numbers: ", 26 === 26);
+
+// • Numerical tests involving inequality 
+console.log("Equality Test with numbers: ", (26 as number) != 36);
+
+// Greater than test
+console.log("Greater than test: ", 10 > 5);
+//  less than test
+console.log("less than test: ", 10 < 5);
+
+// Greater than or equal to test
+console.log("Greater than or equal to test: ", 10 >= 10);
+
+// less than or equal to
+console.log("less than or equal to test: ", 5<= 10);
+
+// • Tests using "and" and "or" operators
+console.log("And operator test: ", 5===5 && 10 > 5);
+
+// • Tests using "and" and "or" operators
+console.log("or operator test: ", 5===5 || false);
+
+// • Test whether an item is in a array
+const fruits : string[] =[`Mango` , `PineApple`, `Amrood`];
+console.log(`test "Amrood" in the array:`, fruits.includes("Amrood"));
+
+// • Test whether an item is not in a array
+console.log(`Testing "Apple" is not an Array: ` , fruits.includes(`Apple`));
+
+
+
+
+
