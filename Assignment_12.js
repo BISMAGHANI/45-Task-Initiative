afff4df6fa277a55ba00b3d7f7eb75bbd911555f
BISMAGHANI <159704908+BISMAGHANI@users.noreply.github.com>
2024-04-28 20:00:16 +0500

    Add files via upload

---
+++ Assignment_12.js
@@ -0,0 +1,6 @@
+"use strict";
+var names = ["Hudebiya", "Haniya", "Humna", "Maryam"];
+var message = "Welcome to Typescript Class;";
+for (var i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ Assignment_12.ts
@@ -0,0 +1,6 @@
+"use strict";
+let names = ["Hudebiya", "Haniya", "Humna", "Maryam"];
+const message = `Welcome to Typescript Class;`;
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
