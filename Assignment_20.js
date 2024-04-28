fde20e804c7ae4ddfed12875c312c1d108aebc6e
BISMAGHANI <159704908+BISMAGHANI@users.noreply.github.com>
2024-04-28 22:01:21 +0500

    Add files via upload

---
+++ Assignment_20.js
@@ -0,0 +1,8 @@
+// Think of something you could store in a array.
+var items = ["Pakistan", "Karachi", "Urdu"];
+// Write a program that creates a list containing these items.
+var item2 = [];
+item2.push("Germany");
+item2.push("Franch");
+item2.push("England");
+console.log(item2);
