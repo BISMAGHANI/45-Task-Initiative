42e757a646f7df679e2664388eec3dd7d7155957
BISMAGHANI <159704908+BISMAGHANI@users.noreply.github.com>
2024-04-28 22:03:40 +0500

    Add files via upload

---
+++ Assignment_22.js
@@ -0,0 +1,9 @@
+"use strict";
+// If you haven’t received an array index error in one of your programs yet, try to make one happen.
+// Creating array
+let newArray = [`James Bond`, `Tom Cruise`, `Bilal Abbas Khan`];
+// Accesing Array by Index
+console.log(newArray[5]);
+// Make sure you correct the array before closing the program
+// Correct the Erray
+console.log(newArray);
