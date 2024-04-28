2da9bf035dd3edcc128075b29f78c8ee2525e845
BISMAGHANI <159704908+BISMAGHANI@users.noreply.github.com>
2024-04-28 20:01:28 +0500

    Add files via upload

---
+++ Assignment_13.js
@@ -0,0 +1,4 @@
+var transportation = ["Motorcycle", "Car", "Train", "Cycle"];
+for (var i = 0; i < transportation.length; i++) {
+    console.log("I Would Like to Own a " + transportation[i]);
+}
---
+++ Assignment_13.ts
@@ -0,0 +1,6 @@
+let transportation: string[]= [`Motorcycle`,`Car`,`Train`,`Cycle`];
+
+for (let i=0; i<transportation.length; i++) {
+
+    console.log(`I Would Like to Own a ` +  transportation[i])
+}
